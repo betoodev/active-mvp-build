79af0e31419d545ca68872abd95bd00c1cff1cc7
chizz7 <ops@betoo.io>
2022-09-08 14:19:26 -0400

    update env

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,13 +3,13 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
-if (!process.env.GOOGLE_CLIENT || !process.env.GOOGLE_SECRET)
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_SECRET)
   throw new Error("Failed to initialize GOOGLE authentication");
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
                    }),
           ],
@@ -19,7 +19,7 @@ export const authOptions: NextAuthOptions = {
     verifyRequest: `/login`,
     error: "/login", // Error code passed in query string as ?error=
   },
-  
+
   adapter: PrismaAdapter(prisma),
   callbacks: {
     session: ({ session, user }) => ({
