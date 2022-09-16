077e49219d399b36a93b1b21ec0785af82306abb
betoodev <112648704+betoodev@users.noreply.github.com>
2022-09-16 23:55:03 +0530

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,8 +5,8 @@ import CredentialsProvider from "next-auth/providers/credentials"
 export default NextAuth({
   providers: [
      GoogleProvider({
-      clientId: '156380673473-ipevg620jqpp9m98o6cedpk6g542ftgf.apps.googleusercontent.com',
-      clientSecret: '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'
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET
         
     }),
   ],
