512f0f656251254236db13132d2317a4dd632126
chizz7 <ops@betoo.io>
2022-09-16 10:34:29 -0400

    comment session

--- pages/api/domain/index.ts
+++ pages/api/domain/index.ts
@@ -10,7 +10,7 @@ export default async function domain(
   res: NextApiResponse
 ) {
   const session = await unstable_getServerSession(req, res, authOptions);
-  if (!session) return res.status(401).end();
+  //if (!session) return res.status(401).end();
 
   switch (req.method) {
     case HttpMethod.POST:
